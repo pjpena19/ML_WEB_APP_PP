a45f1f49fe28eb7a2c0d3cdb74afa10a7fbd1986
pjpena19 <p.j.p.arreaza@outlook.es>
2024-12-01 22:20:01 +0000

    ML_WEB_APP_PP

--- app.py
+++ app.py
@@ -1,37 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import joblib
 import pandas as pd
 
 app = Flask(__name__)
 
-# Carga el modelo entrenado
+# Load the trained model
 model = joblib.load("mymodel.joblib")
 
-# Define las columnas que espera el modelo
+# Define expected model features (adjust as needed)
 features = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def predict():
-    # Obtiene los datos del formulario
+  try:
+    # Get data from JSON request
     data = request.get_json()
-    
-    # Crea un DataFrame con los datos recibidos
-    input_data = pd.DataFrame([data], columns=features)
 
-    # Realiza la predicción
-    prediction = model.predict(input_data)[0]
-
-    # Devuelve la predicción como un string
-    if prediction == 1:
-        result = "La persona tiene un alto riesgo de desarrollar diabetes."
-    else:
-        result = "La persona tiene un bajo riesgo de desarrollar diabetes."
-
-    return result
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Create a DataFrame
+    input_data = pd.
